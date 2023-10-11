e2134b407ff6fb7eadeca0e3ebbe0ea79ca73d0f
aliaryu <aliaryu@yahoo.com>
2023-10-11 12:02:15 +0330

    test database created

--- week13/weather_project/tests/test_database.py
+++ week13/weather_project/tests/test_database.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import MagicMock
+from datetime import datetime, timedelta
 import json
 
 import os
@@ -35,8 +35,39 @@ class TestDataBase(unittest.TestCase):
         saved_response_data = self.db.cursor.fetchall()[0]
         self.assertEqual(saved_response_data, (1, 'something...', 1))
 
-    
+    def test_get_request_count(self):
+        for city in self.app:
+            self.db.save_request_data(city)
+        request_count = self.db.get_request_count()
+        self.assertEqual(request_count, len(self.app))
 
+    def test_get_successful_request_count(self):
+        self.db.save_response_data(1, "something...", 1)
+        self.db.save_response_data(1, "something...", 2)
+        self.db.save_response_data(0, "error message", 3)
+        successful_request_count = self.db.get_successful_request_count()
+        self.assertEqual(successful_request_count, 2)
+
+    def test_get_last_hour_requests(self):
+        now = datetime.now()
+        two_hours_ago = now - timedelta(hours=2)
+        self.db.save_request_data("london")
+        self.db.save_request_data("paris")
+        self.db.save_request_data("new York")
+        query = f"UPDATE request SET request_time = '{two_hours_ago}' WHERE id = 3"
+        self.db.cursor.execute(query)
+        last_hour_requests = self.db.get_last_hour_requests()
+        self.assertEqual([item[0] for item in last_hour_requests], ["london", "paris"])
+
+    def test_get_count_requests_by_city(self):
+        self.db.save_request_data("london")
+        self.db.save_request_data("paris")
+        self.db.save_request_data("tehran")
+        self.db.save_request_data("london")
+        self.db.save_request_data("paris")
+        count_requests_by_city = self.db.get_count_requests_by_city()
+        self.assertEqual(count_requests_by_city, [("london", 2), ("paris", 2), ("tehran", 1)])
+        
 
 if __name__ == '__main__':
     unittest.main()
