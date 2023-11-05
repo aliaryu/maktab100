3150ca7538fa7bd5b41e2f939bdea6579c59f9c6
aliaryu <aliaryu@yahoo.com>
2023-11-06 00:31:24 +0330

    feat : config function added to read .ini files

--- week17/hospital_project/database/dbmanager.py
+++ week17/hospital_project/database/dbmanager.py
@@ -1,4 +1,12 @@
+from configparser import ConfigParser, NoSectionError
 
+
+def config(file_name, section):
+    configs = ConfigParser()
+    configs.read(file_name)
+    if configs.has_section(section):
+        return dict(configs.items(section))
+    raise NoSectionError(f"Section '{section}' not exists in '{file_name}' file")
 
 
 class DBManager:
