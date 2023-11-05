da72761cfb30a14a7f68501d6aafdf6af414e864
aliaryu <aliaryu@yahoo.com>
2023-11-06 00:39:35 +0330

    feat : create add to db and create db methods

--- week17/hospital_project/database/dbmanager.py
+++ week17/hospital_project/database/dbmanager.py
@@ -1,4 +1,5 @@
 from configparser import ConfigParser, NoSectionError
+import psycopg2
 
 
 def config(file_name, section):
@@ -12,9 +13,34 @@ def config(file_name, section):
 # CONFIGS:
 POSTGRES_CONFIG = config("config.ini", "postgres")
 HOSPITAL_CONFIG = config("config.ini", "hospital")
+QUERY_FILE_PATH = "query.sql"
 
 
 class DBManager:
     def __init__(self):
         self.connection = None
         self.cursor = None
+        self.connect()
+
+    def connect(self):
+        try:
+            self.connection = psycopg2.connect(**HOSPITAL_CONFIG)
+            self.cursor = self.connection.cursor()
+        except:
+            self._initialize()
+
+    def _initialize(self):
+        try:
+            temp_connection = psycopg2.connect(**POSTGRES_CONFIG)
+            temp_connection.autocommit = True
+            temp_cursor = temp_connection.cursor()
+            temp_cursor.execute(f"CREATE DATABASE {HOSPITAL_CONFIG['dbname']}")
+        finally:
+            temp_cursor.close()
+            temp_connection.close()
+        self.connection = psycopg2.connect(**HOSPITAL_CONFIG)
+        self.cursor = self.connection.cursor()
+        with open(QUERY_FILE_PATH, "r") as file:
+            queries = file.read()
+        self.execute_query(queries)
+        self.commit_query()
