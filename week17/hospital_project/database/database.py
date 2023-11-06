4de9f4d277251ff5c084b80354f1e540ab84d70c
aliaryu <aliaryu@yahoo.com>
2023-11-06 18:23:37 +0330

    feat : list patients method added to admin

--- week17/hospital_project/database/database.py
+++ week17/hospital_project/database/database.py
@@ -1,1 +1,28 @@
 from dbmanager import DBManager
+
+from datetime import datetime
+
+
+class Admin:
+
+    @staticmethod
+    def list_patients():
+        with DBManager() as db:
+            query = """SELECT fullname, date_of_birth, gender, medical_record_number
+            FROM users u JOIN patients p ON u.user_id = p.user_id LIMIT 50;"""
+            db.execute_query(query)
+            return db.fetch_all()
+        
+
+
+
+
+
+
+
+
+# # Admin.list_patients
+# result = Admin.list_patients()
+# print("fullname".ljust(20), "date_of_birth".ljust(15), "gender".ljust(8), "medical_record_number".ljust(10), "\n")
+# for item in result:
+#     print(item[0].ljust(20), item[1].strftime("%Y-%m-%d").ljust(15), item[2].ljust(8), str(item[3]).ljust(10))
