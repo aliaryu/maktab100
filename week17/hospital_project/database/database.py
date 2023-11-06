889959a22d2bdf933f5a2579c22d3daaf1c06ee2
aliaryu <aliaryu@yahoo.com>
2023-11-06 19:24:04 +0330

    feat : doctor visits info added to admin

--- week17/hospital_project/database/database.py
+++ week17/hospital_project/database/database.py
@@ -21,7 +21,16 @@ class Admin:
             db.execute_query(query)
             return db.fetch_all()
 
-
+    @staticmethod
+    def doctor_visits_info():
+        with DBManager() as db:
+            query = """SELECT fullname, specialization, COUNT(visit_id) AS number_of_visits, SUM(cost)
+            AS income FROM users u JOIN doctors d ON u.user_id = d.user_id JOIN appointments a ON
+            d.doctor_id = a.doctor_id JOIN visits v ON a.appointment_id = v.appointment_id WHERE
+            paid = true GROUP BY fullname, specialization;"""
+            db.execute_query(query)
+            return db.fetch_all()
+        
 
 
 
@@ -36,6 +45,13 @@ class Admin:
 
 # # Admin.list_doctors
 # result = Admin.list_doctors()
-# print("fullname".ljust(20), "date_of_birth".ljust(15), "gender".ljust(8), "specialization".ljust(20), "medical_license_number".ljust(15), "\n")
+# print("fullname".ljust(20), "date_of_birth".ljust(15), "gender".ljust(8), "specialization".ljust(20), "medical_license_number".ljust(20), "\n")
 # for item in result:
-#     print(item[0].ljust(20), item[1].strftime("%Y-%m-%d").ljust(15), item[2].ljust(8), item[3].ljust(20), str(item[4]).ljust(15))
+#     print(item[0].ljust(20), item[1].strftime("%Y-%m-%d").ljust(15), item[2].ljust(8), item[3].ljust(20), str(item[4]).ljust(20))
+
+# # Admin.doctor_visits_info
+# result = Admin.doctor_visits_info()
+# print("fullname".ljust(20), "specialization".ljust(20), "number_of_visits".ljust(20), "income".ljust(10), "\n")
+# for item in result:
+#     print(item[0].ljust(20), item[1].ljust(20), str(item[2]).ljust(20), str(float(item[3])).ljust(10))
+
