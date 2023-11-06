275f8c2f16263a35d88925c1764770d777d0c5e3
aliaryu <aliaryu@yahoo.com>
2023-11-06 17:13:38 +0330

    feat : add show contact function

--- week17/hospital_project/main.py
+++ week17/hospital_project/main.py
@@ -1,2 +1,27 @@
 from menu import Menu, Item
+import os
 
+
+def show_contact():
+    print("Phone:".ljust(10), "+84 32 22 1600")
+    print("Email:".ljust(10), "avicenna@support.com")
+    print("Postal:".ljust(10), "14516167")
+    print("Fax:".ljust(10), "843222")
+    print("Address:".ljust(10), "Milky Way, Solar System, Heaven ;)")
+    input("\nPress 'Enter' to continue ...")
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+
+
+
+
+
+
+
+main = Menu("Avicenna Hospital")
+
+main.add_item(Item("Contact", show_contact))
+
+
+main.execute()
