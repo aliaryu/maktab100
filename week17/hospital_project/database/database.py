6796361df7ae448b621b760243a1b493b73db943
aliaryu <aliaryu@yahoo.com>
2023-11-06 18:45:28 +0330

    fix : list doctors method query bug fixed

--- week17/hospital_project/database/database.py
+++ week17/hospital_project/database/database.py
@@ -13,6 +13,13 @@ class Admin:
             db.execute_query(query)
             return db.fetch_all()
         
+    @staticmethod
+    def list_doctors():
+        with DBManager() as db:
+            query = """SELECT fullname, date_of_birth, gender, specialization, medical_license_number
+            FROM users u JOIN doctors d ON u.user_id = d.user_id LIMIT 50;"""
+            db.execute_query(query)
+            return db.fetch_all()
 
 
 
@@ -26,3 +33,9 @@ class Admin:
 # print("fullname".ljust(20), "date_of_birth".ljust(15), "gender".ljust(8), "medical_record_number".ljust(10), "\n")
 # for item in result:
 #     print(item[0].ljust(20), item[1].strftime("%Y-%m-%d").ljust(15), item[2].ljust(8), str(item[3]).ljust(10))
+
+# # Admin.list_doctors
+# result = Admin.list_doctors()
+# print("fullname".ljust(20), "date_of_birth".ljust(15), "gender".ljust(8), "specialization".ljust(20), "medical_license_number".ljust(15), "\n")
+# for item in result:
+#     print(item[0].ljust(20), item[1].strftime("%Y-%m-%d").ljust(15), item[2].ljust(8), item[3].ljust(20), str(item[4]).ljust(15))
