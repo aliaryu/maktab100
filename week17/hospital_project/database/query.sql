99cb80fb94f4c4a88173ff2c5ce767980c7699ba
aliaryu <aliaryu@yahoo.com>
2023-11-06 13:42:32 +0330

    fix : change int to bigint for integer values

--- week17/hospital_project/database/query.sql
+++ week17/hospital_project/database/query.sql
@@ -1,7 +1,7 @@
 -- Hospital DataBase Queries --
 
 CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
+    user_id BIGSERIAL PRIMARY KEY,
     fullname VARCHAR (100) NOT NULL,
     email VARCHAR (100) UNIQUE NOT NULL,
     date_of_birth DATE NOT NULL,
@@ -17,30 +17,30 @@ CREATE TABLE users (
 );
 
 CREATE TABLE admins (
-    admin_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users (user_id) UNIQUE,
+    admin_id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT REFERENCES users (user_id) UNIQUE,
     position VARCHAR (100) NOT NULL
     -- permission, group, etc ...
 );
 
 CREATE TABLE doctors (
-    doctor_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users (user_id) UNIQUE,
+    doctor_id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT REFERENCES users (user_id) UNIQUE,
     specialization VARCHAR (100) NOT NULL,
-    medical_license_number INT UNIQUE NOT NULL
+    medical_license_number BIGINT UNIQUE NOT NULL
     -- education, university, etc ...
 );
 
 CREATE TABLE patients (
-    patient_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users (user_id),
-    medical_record_number INT UNIQUE NOT NULL
+    patient_id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT REFERENCES users (user_id),
+    medical_record_number BIGINT UNIQUE NOT NULL
     -- blood type, allergy, etc ...
 );
 
 CREATE TABLE appointments (
-    appointment_id SERIAL PRIMARY KEY,
-    doctor_id INT REFERENCES doctors (doctor_id),
+    appointment_id BIGSERIAL PRIMARY KEY,
+    doctor_id BIGINT REFERENCES doctors (doctor_id),
     appointment_date DATE NOT NULL,
     appointment_time TIME NOT NULL,
     cost DECIMAL(10, 2) NOT NULL,
@@ -49,9 +49,9 @@ CREATE TABLE appointments (
 );
 
 CREATE TABLE visits (
-    visit_id SERIAL PRIMARY KEY,
-    appointment_id INT REFERENCES appointments (appointment_id),
-    patient_id INT REFERENCES patients (patient_id),
+    visit_id BIGSERIAL PRIMARY KEY,
+    appointment_id BIGINT REFERENCES appointments (appointment_id),
+    patient_id BIGINT REFERENCES patients (patient_id),
     paid BOOLEAN DEFAULT FALSE NOT NULL
     -- payment methods, bank number, etc ...
     -- OR: create payment table
@@ -71,9 +71,6 @@ INSERT INTO admins (user_id, position) VALUES (2, 'security');
 INSERT INTO users (fullname, email, date_of_birth, gender, username, password, role, superuser, active, delete)
 VALUES ('artmis daryasalar', 'artmis@daryasalar.com', '1996-04-03', 'female', 'artmis', '1', 'admin', FALSE, TRUE, FALSE);
 INSERT INTO admins (user_id, position) VALUES (3, 'financial');
-
-
-
 
 
 
