2c8b9ab2d46674e6b4094a68db12470a01587659
aliaryu <aliaryu@yahoo.com>
2023-11-07 16:58:06 +0330

    perf : improve show sign in function

--- week17/hospital_project/main.py
+++ week17/hospital_project/main.py
@@ -8,11 +8,32 @@ from database.database import User
 import os
 
 
+# USER COOCKIES
+# (1, 'ali aryu', 'aliaryu@yahoo.com', datetime.date(1997, 4, 22), 'male', 'aliaryu', <memory at 0x000001519434E5C0>, 'admin', True, True, False)
+#  0     1               2                       3 hbd                4        5                    6 pw                 7      8s    9a    10d
+user_info = None
+
+
+
 def clear_terminal():
     os.system("cls" if os.name == "nt" else "clear")
 
 def show_sign_in():
-    pass
+    username = input("Username: ")
+    password = input("Password: ")
+    global user_info
+    user_info = User.sign_in(username,password)
+    clear_terminal()
+    if user_info:
+        print(f"Welcome to {user_info[7]} panel '{user_info[1].title()}' :D\n")
+        if user_info[7] == "admin":
+            admin_menu.execute()
+        elif user_info[7] == "doctor":
+            pass
+        elif user_info[7] == "patient":
+            pass
+    else:
+        print("Invalid username or password >_<")
 
 def show_sign_up():
     pass
@@ -58,16 +79,16 @@ def show_about():
 
 
 
+# MAIN MENU
+main_menu = Menu("Avicenna Hospital", exit_message="Your health is our goal <3 Bye.")
+main_menu.add_item(Item("Sign-In", show_sign_in))
+main_menu.add_item(Item("Sign-Up", show_sign_up))
+main_menu.add_item(Item("Contact", show_contact))
+main_menu.add_item(Item("About", show_about))
+
+# ADMIN MENU
+admin_menu = Menu("Admin", "Logout", "Bye Bye ;*")
 
 
-
-
-main = Menu("Avicenna Hospital")
-
-main.add_item(Item("Sign-In", show_sign_in))
-main.add_item(Item("Sign-Up", show_sign_up))
-
-main.add_item(Item("Contact", show_contact))
-main.add_item(Item("About", show_about))
-
-main.execute()
+if __name__ == "__main__":
+    main_menu.execute()
