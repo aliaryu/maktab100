ac786bbd6adc446bde8c3854f175760efc52e076
aliaryu <aliaryu@yahoo.com>
2023-11-09 03:05:19 +0330

    feat : class test dbmanager created with patch

--- week17/hospital_project/tests/test_dbmanager.py
+++ week17/hospital_project/tests/test_dbmanager.py
@@ -1,3 +1,24 @@
+import sys
+from pathlib import Path
+project_folder = Path(__file__).resolve().parent.parent
+sys.path.append(str(project_folder))
+
 import unittest
 from unittest.mock import patch
+from database.dbmanager import DBManager, config
 
+class TestDBManager(unittest.TestCase):
+    def setUp(self):
+        with patch('database.dbmanager.HOSPITAL_CONFIG', config("database/config.ini", "test_hospital")):
+            self.db = DBManager()
+    
+    def tearDown(self) -> None:
+        self.db.close()
+
+    def test_insert(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
