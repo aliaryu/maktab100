20676aeac0729d71d5d7bf994015a08f85617aa8
aliaryu <aliaryu@yahoo.com>
2023-11-08 20:09:29 +0330

    feat : add user loger

--- week17/hospital_project/logger.py
+++ week17/hospital_project/logger.py
@@ -1,8 +1,16 @@
 import logging
 
+# CONFIGS
+OUTPUT_FOLDER  = "logs/"
+HANDLER_FORMAT = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(funcName)s - %(message)s")
 
-
-
+# LOGGER USER
+logger_user = logging.getLogger("database.User")
+file_handler_user = logging.FileHandler(OUTPUT_FOLDER + "user.log")
+file_handler_user.setFormatter(HANDLER_FORMAT)
+file_handler_user.setLevel(logging.INFO)
+logger_user.addHandler(file_handler_user)
+logger_user.setLevel(logging.INFO)
 
 
 
