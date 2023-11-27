bd64d13a78a95fb0dff67d3959a5294fff47920f
aliaryu <aliaryu@yahoo.com>
2023-11-28 03:26:30 +0330

    feat: create task, category, tag models

--- week20/TaskManager/task/models.py
+++ week20/TaskManager/task/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(verbose_name="Category", max_length=50)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.category
+
+
+class Tag(models.Model):
+    tag = models.CharField(verbose_name="Tag", max_length=50)
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
+    def __str__(self):
+        return self.tag
+
+
+def date_validation(value):
+    if value < timezone.now().date():
+        raise ValidationError("Please enter a future date sheytun balaw.")
+
+
+STATUS_CHOICES = [
+        ('I', 'In Progress'),
+        ('C', 'Completed'),
+    ]
+
+
+class Task(models.Model):
+    title       = models.CharField(verbose_name="Title", max_length=50)
+    description = models.TextField(verbose_name="Description")
+    due_date    = models.DateField(verbose_name="Due Date", validators=[date_validation])
+    status      = models.CharField(
+        verbose_name="Status",
+        max_length=1,
+        choices=STATUS_CHOICES,
+        default="I"
+    )
+    category = models.ForeignKey(
+        Category,
+        models.CASCADE,
+        verbose_name="Category",
+        related_name="tasks"
+    )
+    tags = models.ManyToManyField(Tag, verbose_name="Tags", related_name="tasks")
+
+    class Meta:
+        ordering = ["-due_date"]
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+
+    def __str__(self):
+        return self.title
+
