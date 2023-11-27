5ca0b1111b5d431ca63b23ba7dabb856246d67c2
aliaryu <aliaryu@yahoo.com>
2023-11-28 02:44:05 +0330

    init: start app task

--- week20/TaskManager/src/settings.py
+++ week20/TaskManager/src/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # DEFAULT APPS
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # INTERNAL APPS
+    "task.apps.TaskConfig",
+
 ]
 
 MIDDLEWARE = [
