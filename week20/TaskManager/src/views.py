8534868c4c0ad7f73e857d63fd9dbdddd78dff06
aliaryu <aliaryu@yahoo.com>
2023-11-28 16:52:30 +0330

    fix : change redirect after logout

--- week20/TaskManager/src/views.py
+++ week20/TaskManager/src/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def index_view(request):
 
     tasks= 1
-    return render(request, "task/home.html", {'tasks': tasks})
+    return render(request, "task/index.html", {'tasks': tasks})
